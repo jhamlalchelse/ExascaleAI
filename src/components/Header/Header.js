a2b7a3f3d166c6df7d67d1b6a3a40f768706e42c
Jhamlal Chelse <jhamlalchelse@gmail.com>
2024-03-23 11:55:36 +0530

    go home page if logo icon click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,7 +28,7 @@ const Header = () => {
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div className="flex justify-between items-center mx-auto max-w-screen-xl">
-          <Link href="/">
+          <Link to={'/'}>
             <img
               src="assets/images/logo.svg"
               className="mr-3"
